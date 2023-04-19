f5e568eb22b04d6b1bf34aca373ec37133df2071
QuangHieuDev <ngoquanghieu.dev@gmail.com>
2023-04-19 08:10:20 +0700

    create test

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="onehundredvh"></div>
+      <div className="parallax">
+        <div className="">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore,
+          eveniet. Et atque assumenda placeat velit eaque necessitatibus, enim
+          illum molestias vel maxime saepe, eos repellat cum quisquam inventore.
+          Dolores, error. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Inventore, eveniet. Et atque assumenda placeat velit eaque
+          necessitatibus, enim illum molestias vel maxime saepe, eos repellat
+          cum quisquam inventore. Dolores, error. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Inventore, eveniet. Et atque assumenda
+          placeat velit eaque necessitatibus, enim illum molestias vel maxime
+          saepe, eos repellat cum quisquam inventore. Dolores, error. Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Inventore, eveniet.
+          Et atque assumenda placeat velit eaque necessitatibus, enim illum
+          molestias vel maxime saepe, eos repellat cum quisquam inventore.
+          Dolores, error.
+        </div>
+      </div>
+      <div className="onehundredvh"></div>
+    </div>
+  );
+}
+
+export default App;
